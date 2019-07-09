b58f2b164cd065fa42c40dee299debf5609bb9e0
Coreplo <coreplo@gmail.com>
2019-07-10 00:25:43 +0200

    iniz

--- Collections/Program.cs
+++ Collections/Program.cs
@@ -10,12 +10,47 @@ namespace Collections
     {
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
+    
+            Console.WriteLine("Collections");
+
+            // a collection is used to group related objects. unlike array it is dynamic. 
+            // they can grow and shrink to accomodate any numbero of objects 
+            // Collection classes are organized in namespaces with methods to process elements 
+            // within the collection
+
+            //different kinds of collections are suited to different kind of applications
+            // tipically a collection includes Add Remove and Count Methods
+
+            //to use a collention (which, remember, is a class) we must instanciate it
+
+            List<int> li = new List<int>();
+
+            //Generic collections are preferred as long as every element is of the same type
+            //System.Collections.Generic namespace includes the following:
+            List<T>;
+            Dictionary<TKey, TValue>;
+            SortedList<Tkey, TValue>;
+            Stack<T>;
+            Queue<T>;
+            HashSet<T>;
+
+            //Non Generic collections can store items of type Object
+            //System.Collections namespace includes the following:
+            /*
+            ArrayList
+            SortedList
+            Stack
+            Queue
+            HashTable
+            BitArray
+            */
+            //NonGeneric Collections are more error prone and slower in execution. Use Generic Ones if you can
+
+
+
+
             Console.ReadKey();
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
         }
     }
 }
